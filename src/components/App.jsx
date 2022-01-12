2b4ac5a0bbd3c422fdf80510ea5b14f10dd3696b
Alex-Reyne <creativereyne@gmail.com>
2022-01-12 16:50:09 -0600

    create hardcoded navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './Nav.jsx';
 
 function App() {
 	return (
 		<div className='App'>
-			<Portfolio />
+			<Nav />
+			{/* <Portfolio /> */}
 		</div>
 	);
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import './Nav.scss';
+
+export default function Nav() {
+	return (
+		<div className='navbar'>
+			<div className='logo'>
+				<img src='images/alex-reyne-logo-slate.png' alt='Alex Reyne Logo' />
+			</div>
+			<div className='nav-button h'>
+				<h1>+H</h1>
+				<h1 className='selected'>HOME</h1>
+			</div>
+			<div className='nav-button a'>
+				<h1>A</h1>
+			</div>
+			<div className='nav-button w'>
+				<h1>W</h1>
+			</div>
+			<div className='nav-button r'>
+				<h1>R</h1>
+			</div>
+		</div>
+	);
+}
