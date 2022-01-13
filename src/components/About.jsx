172091a2be4f94ab769cc8063c2368f27dd865bc
Alex-Reyne <creativereyne@gmail.com>
2022-01-13 01:50:27 -0600

    add social icons to home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
 export default function About() {
 	return (
-		<div>
-			<h1 className='about-name'>Alexander Reyne</h1>
+		<div className='content'>
+			<div className='home-name'>
+				<h1>Alexander Reyne</h1>
+				<div className='alex-info'>
+					<h2>Full-Stack Web Developer</h2>
+					<h2>Graphic Designer</h2>
+					<hr></hr>
+				</div>
+			</div>
+			<div className='bg-logo'>
+				<img src='images/alex-reyne-logo-gradient.png' alt='Alex Reyne Logo' />
+			</div>
 		</div>
 	);
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,12 @@
+import {
+	faGithubSquare,
+	faLinkedin,
+	faMedium,
+	faInstagramSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Home.scss';
+
 export default function Home() {
 	return (
 		<div className='content'>
@@ -12,6 +20,24 @@ export default function Home() {
 			</div>
 			<div className='bg-logo'>
 				<img src='images/alex-reyne-logo-gradient.png' alt='Alex Reyne Logo' />
+			</div>
+			<div className='home-icons'>
+				<div>
+					<a href='https://github.com/alex-reyne' target='_blank'>
+						<FontAwesomeIcon icon={faGithubSquare} />
+					</a>
+					<a href='https://medium.com/@creativereyne' target='_blank'>
+						<FontAwesomeIcon icon={faMedium} />
+					</a>
+				</div>
+				<div>
+					<a href='https://www.linkedin.com/in/alexanderreyne/' target='_blank'>
+						<FontAwesomeIcon icon={faLinkedin} />
+					</a>
+					<a href='https://www.instagram.com/alexreyne.dev/' target='_blank'>
+						<FontAwesomeIcon icon={faInstagramSquare} />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
