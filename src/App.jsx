63ed61ad52f5e95884e66a1bb750c8fd29fae7d2
Alex-Reyne <creativereyne@gmail.com>
2022-01-29 16:34:34 -0600

    remove "exact" from / path

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 		<div className='content'>
 			<Nav />
 			<Routes>
-				<Route exact path='/' element={<Home />} />
+				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
 				<Route path='/work' element={<Work />} />
 				<Route path='/resume' element={<Resume />} />
