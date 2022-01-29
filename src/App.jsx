4d4c9388abbef4bb0db54dba15a5f9b53582eaa1
Alex-Reyne <creativereyne@gmail.com>
2022-01-29 16:30:55 -0600

    switch from hash router to browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,18 @@ import Work from './Pages/Work.jsx';
 import Resume from './Pages/Resume.jsx';
 import './App.scss';
 import { useState } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div>
-			<HashRouter>
-				<Nav />
-				<div className='content'>
-					<Routes>
-						<Route exact path='/' element={<Home />} />
-						<Route path='/about' element={<About />} />
-						<Route path='/work' element={<Work />} />
-						<Route path='/resume' element={<Resume />} />
-					</Routes>
-				</div>
-			</HashRouter>
+		<div className='content'>
+			<Nav />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/work' element={<Work />} />
+				<Route path='/resume' element={<Resume />} />
+			</Routes>
 		</div>
 	);
 }
