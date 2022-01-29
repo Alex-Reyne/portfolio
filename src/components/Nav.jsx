97ffe086204f60ed389932663dd1b81c1feb78ed
Alex-Reyne <creativereyne@gmail.com>
2022-01-29 15:49:34 -0600

    create selected states for nav-bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react';
 import NavList from './NavList';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Nav.scss';
 
+const HOME = 'home';
+const ABOUT = 'about';
+const WORK = 'work';
+const RESUME = 'resume';
+
 export default function Nav(props) {
+	const [view, setView] = useState(HOME);
+	const location = useLocation();
+
 	return (
 		<nav className='navbar'>
 			<div className='logo'>
 				<img src='images/alex-reyne-logo-slate.png' alt='Alex Reyne Logo' />
 			</div>
 			<div className='link-list'>
-				<NavLink to='/'>
+				<NavLink to='/' onClick={e => setView(HOME)}>
 					<div className='nav-button'>
-						<h1>H</h1>
+						{view === HOME && <h1 className='selected'>+H</h1>}
+						{view !== HOME && <h1>HOME</h1>}
 					</div>
 				</NavLink>
-				<NavLink to='/about'>
+				<NavLink to='/about' onClick={e => setView(ABOUT)}>
 					<div className='nav-button'>
-						<h1>A</h1>
+						{view === ABOUT && <h1 className='selected'>+A</h1>}
+						{view !== ABOUT && <h1>ABOUT</h1>}
 					</div>
 				</NavLink>
-				<NavLink to='/work'>
+				<NavLink to='/work' onClick={e => setView(WORK)}>
 					<div className='nav-button'>
-						<h1>W</h1>
+						{view === WORK && <h1 className='selected'>+W</h1>}
+						{view !== WORK && <h1>WORK</h1>}
 					</div>
 				</NavLink>
-				<NavLink to='/resume'>
+				<NavLink to='/resume' onClick={e => setView(RESUME)}>
 					<div className='nav-button'>
-						<h1>R</h1>
+						{view === RESUME && <h1 className='selected'>+R</h1>}
+						{view !== RESUME && <h1>RESUME</h1>}
 					</div>
 				</NavLink>
 			</div>
