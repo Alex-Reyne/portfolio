fb9ab6d7facd2d6db74ad3bbd57ac9a7652be809
Alex-Reyne <creativereyne@gmail.com>
2022-01-31 17:06:49 -0600

    create resume page

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,9 +1,23 @@
 import Header from '../components/Header';
-
+import './Styles/Resume.scss';
+import EducationDrop from '../components/EducationDrop';
+import SummaryDrop from '../components/SummaryDrop';
+import ExperienceDrop from '../components/ExperienceDrop';
 export default function Resume() {
 	return (
 		<div className='content'>
-			<Header />
+			<div>
+				<Header />
+			</div>
+			<div className='resume-content'>
+				<div className='left-column'>
+					<EducationDrop />
+					<SummaryDrop />
+				</div>
+				<div className='right-column'>
+					<ExperienceDrop />
+				</div>
+			</div>
 		</div>
 	);
 }
